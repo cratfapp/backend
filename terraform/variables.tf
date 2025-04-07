2fc50b102a6e2885ee6def0c8143aabe264aa7dc
Zabih Keraam <165507052+Zabihkeraam1@users.noreply.github.com>
2025-04-07 14:55:05 +0430

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,38 @@
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS Access Key ID"
+variable "customer_name" {
   type        = string
-  sensitive   = true
+  description = "Name of the customer/project"
 }
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS Secret Access Key"
+variable "github_org_name" {
   type        = string
-  sensitive   = true
+  description = "GitHub organization name"
+}
+
+variable "github_repo_name" {
+  type        = string
+  description = "GitHub repository name"
+}
+
+variable "github_branch" {
+  type        = string
+  description = "GitHub branch to deploy from"
+  default     = "main"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region for deployment"
+  default     = "us-east-1"
+}
+
+variable "apprunner_cpu" {
+  type        = string
+  description = "App Runner CPU allocation"
+  default     = "1024"
+}
+
+variable "apprunner_memory" {
+  type        = string
+  description = "App Runner memory allocation"
+  default     = "2048"
 }
